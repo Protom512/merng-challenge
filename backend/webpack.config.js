a4df59789c49078cde8f1fdf7d587a2e800245cc
protom512 <ghouno05@gmail.com>
2021-01-17 15:12:13 +0900

    move to backend

---
+++ backend/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
