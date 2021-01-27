a436e50e18eb58e28eae5c2d23945db5dab6cf7a
Protom512 <ghouno05@gmail.com>
2021-01-27 22:56:09 +0900

    read PORT from env vars (#7)

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
 const { MONGODB_URL } = require('./config');
+const { PORT } = require('./config');
 const pubsub = new PubSub();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req, pubsub }),
 });
-
 mongoose
   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.debug('Mongodb Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
